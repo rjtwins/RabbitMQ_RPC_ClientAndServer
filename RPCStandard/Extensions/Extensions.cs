da99f06c25d360e8b7f73c47465f9bbdca9c80e2
Jorre Vedder <jorre.vedder@flightmap.com>
2023-03-18 09:24:35 +0100

    Re enabled the HostBuilder extension method.

--- RPCStandard/Extensions/Extensions.cs
+++ RPCStandard/Extensions/Extensions.cs
@@ -1,46 +1,44 @@
-﻿//using Microsoft.Extensions.Hosting;
-//using Microsoft.Extensions.DependencyInjection;
-//using RPC.Services;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using RPC.Services;
 
-//namespace RPC.Extensions
-//{
-//    public static class Extensions
-//    {
-//        /// <summary>
-//        /// Add and interface to CoreMap/Flightmap with all the dependent services.
-//        /// The ICoreMapInterface service is the entry point to CoreMap/Flightmap.
-//        /// </summary>
-//        /// <param name="builder"></param>
-//        /// <returns>IHostBuilder builder</returns>
-//        public static IHostBuilder AddRabbitMQRPC(this IHostBuilder builder, string rabbitMQUri = "")
-//        {
-//            if (string.IsNullOrEmpty(rabbitMQUri))
-//            {
-//                builder.ConfigureServices((c, s) =>
-//                {
-//                    s.AddSingleton<IRabbitMQRPCClient, RabbitMQRPCClient>();
-//                    s.AddSingleton<IRabbitMQRPCServer, RabbitMQRPCServer>();
-//                });
-//                return builder;
-//            }
+namespace RPC.Extensions
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Add and interface to CoreMap/Flightmap with all the dependent services.
+        /// The ICoreMapInterface service is the entry point to CoreMap/Flightmap.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns>IHostBuilder builder</returns>
+        public static IHostBuilder AddRabbitMQRPC(this IHostBuilder builder, string rabbitMQUri = "")
+        {
+            if (string.IsNullOrEmpty(rabbitMQUri))
+            {
+                return builder.ConfigureServices((c, s) =>
+                {
+                    s.AddSingleton<IRabbitMQRPCClient, RabbitMQRPCClient>();
+                    s.AddSingleton<IRabbitMQRPCServer, RabbitMQRPCServer>();
+                });
+            }
 
-//            builder.ConfigureServices((c, s) =>
-//            {
-//                s.AddSingleton<IRabbitMQRPCClient>(x => 
-//                {
-//                    var client = new RabbitMQRPCClient();
-//                    client.SetRabbitMQURI(rabbitMQUri);
-//                    return client;
-//                });
+            return builder.ConfigureServices((c, s) =>
+            {
+                s.AddSingleton<IRabbitMQRPCClient>(x =>
+                {
+                    var client = new RabbitMQRPCClient();
+                    client.SetRabbitMQURI(rabbitMQUri);
+                    return client;
+                });
 
-//                s.AddSingleton<IRabbitMQRPCServer>(x =>
-//                {
-//                    var server = new RabbitMQRPCServer();
-//                    server.SetRabbitMQURI(rabbitMQUri);
-//                    return server;
-//                });
-//            });
-//            return builder;
-//        }
-//    }
-//}
+                s.AddSingleton<IRabbitMQRPCServer>(x =>
+                {
+                    var server = new RabbitMQRPCServer();
+                    server.SetRabbitMQURI(rabbitMQUri);
+                    return server;
+                });
+            });
+        }
+    }
+}
